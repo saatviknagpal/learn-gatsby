c30326c3f107da10da040b951e825a3d1dbc6330
Saatvik Nagpal <saatvikngpl21@gmail.com>
2021-09-22 21:39:29 +0530

    Intitial Commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function about() {
+    return (
+        <Layout>
+        <div>
+            <h1>About Page</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit sed ad atque. Inventore ducimus consequatur veniam dicta, facilis expedita, nemo nam, provident autem blanditiis alias architecto ipsum similique suscipit rerum?
+        </div>
+        </Layout>
+    )
+}
