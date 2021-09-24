983e6250505e099a2df8f0fd38ddf8a2dece939d
Saatvik Nagpal <saatvikngpl21@gmail.com>
2021-09-24 21:42:10 +0530

    Added Static Queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
 
 export default function Home() {
+
+
   return(
     <Layout>
     <section className = {styles.header}>
